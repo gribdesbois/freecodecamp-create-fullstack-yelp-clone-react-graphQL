2bd61f144afe6da6330bb7b09ef248fedef77756
gribdesbois <gribdesbois@gmail.com>
2022-02-03 13:34:41 +0100

    added provided source

---
+++ PROVIDED_SOURCE_WORKING/yelp-clone-react/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { render } from "react-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/link-ws";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PlanetSearch from "./components/PlanetSearch";
+import Planet from "./components/Planet";
+import Logo from "./components/shared/Logo";
+import "./index.css";
+
+const GRAPHQL_ENDPOINT = "yelp-clone-freecodecamp-tuto.herokuapp.com/v1/graphql";
+
+const httpLink = new HttpLink({
+  uri: `https://${GRAPHQL_ENDPOINT}`,
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${GRAPHQL_ENDPOINT}`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink,
+});
+
+const App = () => (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Logo />
+      <Switch>
+        <Route path="/planet/:id" component={Planet} />
+        <Route path="/" component={PlanetSearch} />
+      </Switch>
+    </ApolloProvider>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById("root"));
